b7d7c999c4c38abf44fbe9615c89bda08e78abb4
Eric Lease <lease.eric@gmail.com>
2018-02-15 21:34:01 -0500

    v0.0.8 - Add support for Vue component and property decorators - Suppress experimental decorators warning and prevent TS compilation by MSBUILD

--- EricLease/src/vue/index.ts
+++ EricLease/src/vue/index.ts
@@ -1,19 +1,24 @@
 ﻿import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'font-awesome/css/font-awesome.css';
-import Vue from 'vue';
-import HelloComponent from './components/Hello.vue';
+import Vue from "vue";
+import HelloComponent from "./components/Hello.vue";
+import HelloDecoratorComponent from "./components/HelloDecorator.vue";
 
 let v = new Vue({
-    el: '#app',
+    el: "#app",
     template: `
     <div>
         Name: <input v-model="name" type="text">
+        <h1>Hello Component</h1>
         <hello-component :name="name" :initialEnthusiasm="5" />
-    </div>
+        <h1>Hello Decorator Component</h1>
+        <hello-decorator-component :name="name" :initialEnthusiasm="5" />
+        </div>
     `,
-    data: { name: 'World' },
+    data: { name: "World" },
     components: {
-        HelloComponent
+        HelloComponent,
+        HelloDecoratorComponent
     }
 });
